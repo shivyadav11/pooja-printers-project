b5b4077ee06a0df5351ab97140c2c3cce8ff6e25
Shiv Yadav <SHivkumaryadav10012004@gmail.com>
2026-02-13 09:49:58 +0530

    Updated backend live URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pooja-printers-backend.onrender.com/api",
 });
 
 // Auto add token in headers
